cfc60b7065ef6865bd2cf05048122339df2aa7b8
Anselm <afoongwl215@gmail.com>
2015-10-15 04:03:17 +0800

    Code cleanup

--- detector.py
+++ detector.py
@@ -1,13 +1,13 @@
 import pygraphviz as pgv
 import sys
 
-#dfile = "small_flows/flow51005"
+# dfile = "small_flows/flow51005"
 
 
 def fetchInstruction(g):
 	ret = []
 	for i in g.iternodes():
-		if not i.startswith("R_"): ## For bug in sliceall where some register is box
+		if not i.startswith("R_"):  # # For bug in sliceall where some register is box
 			if i.attr["shape"] == "box":
 				ret.append(i.attr["label"])
 	return ret
@@ -16,59 +16,59 @@ def getPattern(g):
 	insns = fetchInstruction(g)
 	ret = []
 	for insn in insns:
-		#[xxxx] 0x1234: movl xyz xyu
-		try: 
-			insn = insn.split(" ",3)
+		# [xxxx] 0x1234: movl xyz xyu
+		try:
+			insn = insn.split(" ", 3)
 			ret.append(insn[2])
 		except:
 			print insns
 			print g
-			#print g.draw("err.png","png","dot")
+			# print g.draw("err.png","png","dot")
 			raise Exception("Unable to find pattern for ")
 
 	return tuple(ret)
 class GadgetType:
-	Movement, Calculation, Setter,Unknown = range(4)
+	Movement, Calculation, Setter, Unknown = range(4)
 class GadgetClass:
-	Basic, NonBasic, Constant, Conditional , Unclassified,Unknown = range(6)
+	Basic, NonBasic, Constant, Conditional , Unclassified, Unknown = range(6)
 
 def classifier(gadget):
-	instructions = fetchInstruction(gadget)
-
-	#basic setter detect movl xxx (eax) -> mem
-	if len(instructions) == 1 and "movl" in instructions[0]:
-		return (GadgetType.Setter,GadgetClass.Constant)
-	
-	#basic adder addl xxx (eax) -> mem
-	if len(instructions) == 1 and "addl" in instructions[0]:
-		return (GadgetType.Calculation,GadgetClass.Constant)
-
-	#detect basic movement mem -> insn -> reg -> insn -> mem
-	if len(instructions) == 2 and all("movl" in instruction for instruction in instructions):
-		return (GadgetType.Movement,GadgetClass.Basic)
 
 	pattern = getPattern(gadget)
-	
-	#basic adder  mem -> movl -> reg -> add xxx reg -> reg -> movl -> mem	
-	if len(pattern) == 3 and pattern in [
+	instructions = fetchInstruction(gadget)
+
+	# basic setter detect movl xxx (eax) -> mem
+	if pattern == ('movl',):
+		return (GadgetType.Setter, GadgetClass.Constant)
+
+	# basic adder addl xxx (eax) -> mem
+	if pattern == ('addl',):
+		return (GadgetType.Calculation, GadgetClass.Constant)
+
+	# detect basic movement mem -> insn -> reg -> insn -> mem
+	if pattern == ('movl', 'movl'):
+		return (GadgetType.Movement, GadgetClass.Basic)
+
+	# basic adder  mem -> movl -> reg -> add xxx reg -> reg -> movl -> mem
+	if pattern in [
 		('movl', 'add', 'movl'),
 		(u'movl', u'and', u'movl')
 										]:
-		return (GadgetType.Calculation,GadgetClass.Basic)
-	
+		return (GadgetType.Calculation, GadgetClass.Basic)
+
 	# mem -> subl -> reg -> addl -> mem
 	if pattern in [
 		(u'addl', u'subl') ,
 		(u'adcl', u'sbbl')
 										]:
-		return (GadgetType.Calculation,GadgetClass.NonBasic)
+		return (GadgetType.Calculation, GadgetClass.NonBasic)
 
-	if len(pattern) == 3 and pattern == ('movl', 'cmovz', 'mov'):
-		return (GadgetType.Movement,GadgetClass.Conditional)
+	if pattern == ('movl', 'cmovz', 'mov'):
+		return (GadgetType.Movement, GadgetClass.Conditional)
 
 	if pattern == (u'movl', u'xor'):
-		return (GadgetType.Setter,GadgetClass.Constant)
-	
+		return (GadgetType.Setter, GadgetClass.Constant)
+
 	# unclassified = [
 	# 	(u'movl', u'cmovz'),
 	# 	(u'movl', u'or', u'movzx', u'or', u'setz', u'setz', u'add', u'setz'),
@@ -93,43 +93,22 @@ def classifier(gadget):
 	# if pattern in unclassified:
 	# 	return (GadgetType.Movement,GadgetClass.Unclassified)
 
-	# movlCnt = 0
-	# addCnt = 0
-	# asmcnt = {}
+	return (GadgetType.Unknown, GadgetClass.Unknown)
 
-	# instructionTypes = ["movl","add"]
-	# for instruction in instructions:
-	# 	for insnType in instructionTypes:
-	# 		if insnType in instruction:
-	# 			asmcnt[insnType] = asmcnt.get(insnType,0)+1
-
-
-	# #basic adder  mem -> movl -> reg -> add xxx reg -> reg -> movl -> mem
-	# if len(instructions) == 3 and asmcnt["movl"] == 2 and asmcnt["add"] ==1:
-	# 	return (GadgetType.Calculation,GadgetClass.Basic)
-
-		
-
-
-
-
-
-	return (GadgetType.Unknown,GadgetClass.Unknown)
-
-def displayGadget(dfile,output="b.png",min_instr = 0,min_mem_in = 0,count=False,inspect=False):
+def displayGadget(dfile, output="b.png", min_instr=0, min_mem_in=0, count=False, inspect=False):
 	g = pgv.AGraph(dfile)
 
 
 	visited = {}
 	stack = []
 	queue = []
-	#Look for all tail node
+	# Look for all tail node
 	numGadget = 0
 	gadgets = []
 	for data_vtx in g.in_edges_iter():
-		gadget = pgv.AGraph(directed=True,strict=False)
+		gadget = pgv.AGraph(directed=True, strict=False)
 		gadget.node_attr.update(shape="box")
-		#gadgetcol = []
+		# gadgetcol = []
 
 		parent = data_vtx[0]
 		child = data_vtx[1]
@@ -145,59 +124,66 @@ def displayGadget(dfile,output="b.png",min_instr = 0,min_mem_in = 0,count=False,
 		# if g.in_degree(parent) == 0 or not g.in_edges(parent)[0][0].startswith("R"):
 			# continue
 
-		#print data_vtx
-		#gadgetcol.append(data_vtx)
-		gadget.add_node(g.get_node(parent).name,**g.get_node(parent).attr)
-		gadget.add_node(g.get_node(child).name,**g.get_node(child).attr)
-		gadget.add_edge(parent,child,None,**data_vtx.attr)
+		# print data_vtx
+		# gadgetcol.append(data_vtx)
 
-		#Traverse up to split
+		t_parent_node = g.get_node(parent)
+		t_child_node = g.get_node(child)
+		gadget.add_node(t_parent_node.name, **t_parent_node.attr)
+		gadget.add_node(t_child_node.name, **t_child_node.attr)
+		gadget.add_edge(parent, child, None, **data_vtx.attr)
+
+		# Traverse up to split
 		hasMemInPreRead = False
-		rootinsn = [[g.get_node(parent),g.get_node(parent)]]
-		leafinsn = g.get_node(parent)
+		rootinsn = [[t_parent_node, t_parent_node]]
+		leafinsn = t_parent_node
 		while g.in_degree(parent) == 1:
 			rootinsn = None
 			parent_edge = g.in_edges(parent)[0]
 			parent = parent_edge[0]
-			gadget.add_node(g.get_node(parent).name,**g.get_node(parent).attr)
-			gadget.add_edge(parent_edge[0],parent_edge[1],None,**parent_edge.attr)
-			#gadgetcol.append(parent_edge)
+
+			t_parent_node = g.get_node(parent)
+			gadget.add_node(t_parent_node.name, **t_parent_node.attr)
+			gadget.add_edge(parent_edge[0], parent_edge[1], None, **parent_edge.attr)
+			# gadgetcol.append(parent_edge)
 
 			# print "pr",parent
 			if parent.startswith("mem_"):
 				hasMemInPreRead = True
 				if g.in_degree(parent) != 0:
-					rootinsn = [ [g.get_node(g.in_edges(parent)[0][0]),g.get_node(parent_edge[1]) ]]
+					rootinsn = [ [g.get_node(g.in_edges(parent)[0][0]), g.get_node(parent_edge[1]) ]]
 				else:
-					rootinsn = [[g.get_node(parent_edge[0]),g.get_node(parent_edge[1])]]
+					rootinsn = [[g.get_node(parent_edge[0]), g.get_node(parent_edge[1])]]
 
 
 
 				# print "mem in pr"
 				break
 
-			#gadget.add_edge(parent_edge)
+			# gadget.add_edge(parent_edge)
 
 		if rootinsn is None:
 			if g.in_degree(parent) == 0:
-				rootinsn = [[g.get_node(parent),g.get_node(parent)]]
+				t_parent_node = g.get_node(parent)
+				rootinsn = [[t_parent_node, t_parent_node]]
 			else:
 				rootinsn = []
-			
+
 		# print gadget
 
-		
 
-		#print parent,child
-		#print "gc",gadgetcol
-		#continue
-		#at the point where we have input from 2 memory loc
-		def followToMemLoad(edge,g,gadget):
+
+		# print parent,child
+		# print "gc",gadgetcol
+		# continue
+		# at the point where we have input from 2 memory loc
+		def followToMemLoad(edge, g, gadget):
 			parent = edge[0]
 			child = edge[1]
-
-			gadget.add_node(g.get_node(parent).name,**g.get_node(parent).attr)
-			gadget.add_edge(parent,child,None,**edge.attr)
+			t_parent_node = g.get_node(parent)
+			c_node = g.get_node(child)
+			gadget.add_node(t_parent_node.name, **t_parent_node.attr)
+			gadget.add_edge(parent, child, None, **edge.attr)
 
 
 			# print parent,child
@@ -210,22 +196,24 @@ def displayGadget(dfile,output="b.png",min_instr = 0,min_mem_in = 0,count=False,
 						# print "asdsad",edge2
 						break
 				if hasRegOp:
-					gadget.add_node(g.get_node(edge2[0]).name,**g.get_node(edge2[0]).attr)
-					gadget.add_edge(edge2[0],child,None,**edge.attr)
+					t_node = g.get_node(edge2[0])
+
+					gadget.add_node(t_node.name, **t_node.attr)
+					gadget.add_edge(edge2[0], child, None, **edge.attr)
 					if g.in_degree(parent) != 0:
-			 			return -1,[[g.get_node(g.in_edges(parent)[0][0]),g.get_node(child)],[g.get_node(edge2[0]),g.get_node(child)]]
+			 			return -1, [[g.get_node(g.in_edges(parent)[0][0]), c_node], [t_node, c_node]]
 			 		else:
-			 			return -1,[[g.get_node(child),g.get_node(child)],[g.get_node(edge2[0]),g.get_node(child)]]
+			 			return -1, [[c_node, c_node], [t_node, c_node]]
 				# print g.in_edges(parent)[0]
 
 				if g.in_degree(parent) != 0:
-					return -1,[[g.get_node(g.in_edges(parent)[0][0]),g.get_node(child)]]
+					return -1, [[g.get_node(g.in_edges(parent)[0][0]), c_node]]
 				else:
-					return -1,[[g.get_node(child),g.get_node(child)]]
+					return -1, [[c_node, c_node]]
 
 
 			# print "apple"
-			#noMemLoad = 0
+			# noMemLoad = 0
 			rval = 0
 			parentCnt = 0
 			rootinsn = []
@@ -237,29 +225,29 @@ def displayGadget(dfile,output="b.png",min_instr = 0,min_mem_in = 0,count=False,
 
 				visited2[edge[0]] = True
 
-				#$print edge
-				arval,insn = followToMemLoad(edge,g,gadget)
+				# $print edge
+				arval, insn = followToMemLoad(edge, g, gadget)
 				if arval == -1:
-					return 0,insn
+					return 0, insn
 				rval += arval
 				parentCnt += 1
 				rootinsn += insn
 
-			
+
 			if parentCnt == 0:
-				return 1,[[g.get_node(parent),g.get_node(parent)]]
-			return rval, rootinsn #if not len(rootinsn) == 1 else rootinsn[0]
+				return 1, [[t_parent_node, t_parent_node]]
+			return rval, rootinsn  # if not len(rootinsn) == 1 else rootinsn[0]
 
 
 		if not hasMemInPreRead:
-			#rootinsn = []
+			# rootinsn = []
 			forkHasNoMemLoad = 1
 			for fork_edge in g.in_edges_iter(parent):
-				forkHasNoMemLoad,rinsn = followToMemLoad(fork_edge,g,gadget)
+				forkHasNoMemLoad, rinsn = followToMemLoad(fork_edge, g, gadget)
 				rootinsn += rinsn
 				# print forkHasNoMemLoad
 				if forkHasNoMemLoad == -1:
-					#rootinsn += rinsn
+					# rootinsn += rinsn
 					break
 				# rootinsn += rinsn
 					# raise Exception("get -1")
@@ -276,7 +264,7 @@ def displayGadget(dfile,output="b.png",min_instr = 0,min_mem_in = 0,count=False,
 
 			# if forkHasNoMemLoad >0 : continue
 
-		##filtering
+		# #filtering
 		RegInFilter = ["R_ESP_0_pre"]
 		if len(rootinsn) == 1 and rootinsn[0][0] in RegInFilter:
 			continue
@@ -291,12 +279,12 @@ def displayGadget(dfile,output="b.png",min_instr = 0,min_mem_in = 0,count=False,
 		if len(rootinsn) == 1 and any(f in rootinsn[0][1].attr["label"] for f in FirstInsnFilter):
 			continue
 
-		instructions = fetchInstruction(gadget)	
+		instructions = fetchInstruction(gadget)
 		insnException = ["calll"]
 		if any(keyword in instruction for keyword in insnException for instruction in instructions):
 			continue
 
-		#print rootinsn,leafinsn
+		# print rootinsn,leafinsn
 
 		if min_instr or min_mem_in or count:
 			instrCnt = 0
@@ -312,28 +300,26 @@ def displayGadget(dfile,output="b.png",min_instr = 0,min_mem_in = 0,count=False,
 			if min_mem_in and mem_in_cnt < min_mem_in:
 				continue
 			if count:
-				print "instrCnt",instrCnt
-				print "min_mem_in",min_mem_in
+				print "instrCnt", instrCnt
+				print "min_mem_in", min_mem_in
 
 		numGadget += 1
 		gclass = classifier(gadget)
-		gadgets.append([gadget,gclass,rootinsn,leafinsn])
-
-		
-
+		gadgets.append([gadget, gclass, rootinsn, leafinsn])
 
 		if inspect:
 			# if gclass == (GadgetType.Unknown,GadgetClass.Unknown):
-			gadget.draw(output,"png","dot")
+			gadget.draw(output, "png", "dot")
 			print "Press enter to load next gadget"
 			raw_input()
 			# else:
 			# 	print "class",gclass
-	return numGadget,gadgets
+	g.clear()
+	return numGadget, gadgets
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		sys.exit("a.py <dotfile>")
 
 	dfile = sys.argv[1]
-	print displayGadget(dfile,inspect=True)
+	print displayGadget(dfile, inspect=True)
--- processAll.py
+++ processAll.py
@@ -1,21 +1,21 @@
 import pygraphviz as pgv
-import sys,os,shutil
+import sys, os, shutil
 import multiprocessing
 import itertools
 
-from detector import displayGadget,GadgetType,GadgetClass,fetchInstruction,getPattern
+from detector import displayGadget, GadgetType, GadgetClass, fetchInstruction, getPattern
 
 seenPattern = {}
 cnt = 0
-def process(flowfile,moveNChosen=False,dfolder="",moveFolder = "NoGadget",inspect = False):
-	#print flowfile
+def process(flowfile, moveNChosen=False, dfolder="", moveFolder="NoGadget", inspect=False):
+	# print flowfile
 	global cnt
 	global seenPattern
-	
+
 	cnt += 1
 	if not cnt % 1000:
-		print cnt
-	#print len(g.edges()),g.edges()
+		print cnt, flowfile
+	# print len(g.edges()),g.edges()
 	# if len(g.edges()) < 3:
 	# 	continue
 
@@ -37,17 +37,17 @@ def process(flowfile,moveNChosen=False,dfolder="",moveFolder = "NoGadget",inspec
 	# 	from guppy import hpy
 	# 	h = hpy()
 	# 	print h.heap()
-	
-	g = pgv.AGraph(flowfile)
 
-	num_edge = len(g.edges())
+	# g = pgv.AGraph(flowfile)
 
-	if num_edge<2: #implies to register
-		return 0
+	# num_edge = len(g.edges())
+
+	# if num_edge<2: #implies to register
+	# 	return 0
 
 
-	if num_edge == 2 and all(("EIP" in e[0] or "EIP" in e[1]) for e in g.iteredges()): #skip jne/je/jz
-		return 0
+	# if num_edge == 2 and all(("EIP" in e[0] or "EIP" in e[1]) for e in g.iteredges()): #skip jne/je/jz
+	# 	return 0
 
 
 		# memCnt = 0
@@ -58,23 +58,23 @@ def process(flowfile,moveNChosen=False,dfolder="",moveFolder = "NoGadget",inspec
 		# if memCnt < 1:
 		# 	return 0
 
-		
-		#raw_input()	
+
+		# raw_input()
 	try:
-		numGadget,gadgets = displayGadget(flowfile,"b.png",min_instr=0,min_mem_in = 0, count = False,inspect=False)
+		numGadget, gadgets = displayGadget(flowfile, "b.png", min_instr=0, min_mem_in=0, count=False, inspect=False)
 		if inspect and numGadget:
-			if any((GadgetType.Unknown,GadgetClass.Unknown)== gclass for _,gclass,_,_ in gadgets):
-				g.draw("a.png","png","dot")
-			for gadget,gclass,rootinsn,leafinsn in gadgets:
-				print rootinsn,leafinsn
-				if gclass == (GadgetType.Unknown,GadgetClass.Unknown):
+			if any((GadgetType.Unknown, GadgetClass.Unknown) == gclass for _, gclass, _, _ in gadgets):
+				g.draw("a.png", "png", "dot")
+			for gadget, gclass, rootinsn, leafinsn in gadgets:
+				print rootinsn, leafinsn
+				if gclass == (GadgetType.Unknown, GadgetClass.Unknown):
 				# if 1==1:
-					
-					
+
+
 					pat = getPattern(gadget)
-					seenPattern[pat] = seenPattern.get(pat,0)+1
-					if seenPattern.get(pat,0) == 1:
-						gadget.draw("b.png","png","dot")
+					seenPattern[pat] = seenPattern.get(pat, 0) + 1
+					if seenPattern.get(pat, 0) == 1:
+						gadget.draw("b.png", "png", "dot")
 						# g.draw("a.png","png","dot")
 						print fetchInstruction(gadget)
 						print ""
@@ -86,53 +86,53 @@ def process(flowfile,moveNChosen=False,dfolder="",moveFolder = "NoGadget",inspec
 
 		if not numGadget:
 			if moveNChosen:
-				dstFolder = os.path.join(dfolder,"fail")
+				dstFolder = os.path.join(dfolder, "fail")
 				if not os.path.exists(dstFolder):
 					os.makedirs(dstFolder)
-				shutil.move(flowfile,dstFolder)
+				shutil.move(flowfile, dstFolder)
 			return 0
 		else:
 			if moveNChosen:
-				dstFolder = os.path.join(dfolder,"pass")
-			if not os.path.exists(dstFolder):
-				os.makedirs(dstFolder)
-			shutil.move(flowfile,dstFolder)
+				dstFolder = os.path.join(dfolder, "pass")
+				if not os.path.exists(dstFolder):
+					os.makedirs(dstFolder)
+				shutil.move(flowfile, dstFolder)
 
-		
+
 		if inspect:
-			print flowfile.rsplit("/")[-1].rsplit("\\")[-1],numGadget, len([ga for ga in g.nodes() if ga.attr["shape"]=="box"]), [len([ga for gb in ga[0].nodes() if gb.attr["shape"]=="box"]) for ga in gadgets]
-		
+			print flowfile.rsplit("/")[-1].rsplit("\\")[-1], numGadget, len([ga for ga in g.nodes() if ga.attr["shape"] == "box"]), [len([ga for gb in ga[0].nodes() if gb.attr["shape"] == "box"]) for ga in gadgets]
+
 		if inspect:
 			print "Done processing flow <Enter> for next flow"
-			#raw_input()
+			# raw_input()
 		return numGadget
 	except Exception, e:
-		print "errerr",flowfile
+		print "errerr", flowfile
 		print e
 		import traceback
 		traceback.print_exc()
-		#raise Exception()
-		errFolder = os.path.join(dfolder,"error")
+		# raise Exception()
+		errFolder = os.path.join(dfolder, "error")
 		if not os.path.exists(errFolder):
 			os.makedirs(errFolder)
-		shutil.move(flowfile,errFolder)
+		shutil.move(flowfile, errFolder)
 		print "file moved to", errFolder
 
 
 def process_star(inpt):
-	#print 
+	# print
 	inpt = (inpt[0],) + inpt[1]
 	return process(*inpt)
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		sys.exit("a.py <dotfile>")
-	
+
 
 	moveNChosen = True
 	moveFolder = "NoGadget"
 	inspect = False
-	
+
 
 	lp = 0
 	totalNumGadgets = 0
@@ -141,27 +141,27 @@ if __name__ == '__main__':
 	for i in os.listdir(dfolder):
 		if i.startswith("flow"):
 			# print i
-			flowfile = os.path.join(dfolder,i)
+			flowfile = os.path.join(dfolder, i)
 			try:
-				totalNumGadgets += process(flowfile,moveNChosen,dfolder,moveFolder,inspect)
+				totalNumGadgets += process(flowfile, moveNChosen, dfolder, moveFolder, inspect)
 			except Exception, e:
-				print "errerr",i
+				print "errerr", i
 				print e
 				import traceback
 				traceback.print_exc()
-				
-				
-				
+
+
+
 			# q.append(flowfile)
 
 	# pool = multiprocessing.Pool(1)
 	# #http://stackoverflow.com/a/28463266
 
-	#for k in itertools.izip(q, itertools.repeat((str(moveNChosen),dfolder,moveFolder))):
+	# for k in itertools.izip(q, itertools.repeat((str(moveNChosen),dfolder,moveFolder))):
 	# #	print k
 	# result = pool.map(process_star,itertools.izip(q, itertools.repeat((moveNChosen,dfolder,moveFolder,inspect))))
 	# pool.close()
 	# pool.join()
 
 	# totalNumGadgets = sum(result)
-	print "totalNumGadgets",totalNumGadgets
+	print "totalNumGadgets", totalNumGadgets
