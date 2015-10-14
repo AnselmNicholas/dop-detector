a95947dac6389aa7889125d06d8f0facca395788
Anselm <afoongwl215@gmail.com>
2015-10-15 03:24:00 +0800

    Error processing

--- processAll.py
+++ processAll.py
@@ -6,7 +6,15 @@ import itertools
 from detector import displayGadget,GadgetType,GadgetClass,fetchInstruction,getPattern
 
 seenPattern = {}
+cnt = 0
 def process(flowfile,moveNChosen=False,dfolder="",moveFolder = "NoGadget",inspect = False):
+	#print flowfile
+	global cnt
+	global seenPattern
+	
+	cnt += 1
+	if not cnt % 1000:
+		print cnt
 	#print len(g.edges()),g.edges()
 	# if len(g.edges()) < 3:
 	# 	continue
@@ -52,52 +60,69 @@ def process(flowfile,moveNChosen=False,dfolder="",moveFolder = "NoGadget",inspec
 
 		
 		#raw_input()	
-	numGadget,gadgets = displayGadget(flowfile,"b.png",min_instr=0,min_mem_in = 0, count = False,inspect=False)
-	if inspect and numGadget:
-		if any((GadgetType.Unknown,GadgetClass.Unknown)== gclass for _,gclass,_,_ in gadgets):
-			g.draw("a.png","png","dot")
-		for gadget,gclass,rootinsn,leafinsn in gadgets:
-			print rootinsn,leafinsn
-			if gclass == (GadgetType.Unknown,GadgetClass.Unknown):
-			# if 1==1:
-				
-				
-				pat = getPattern(gadget)
-				seenPattern[pat] = seenPattern.get(pat,0)+1
-				if seenPattern.get(pat,0) == 1:
-					gadget.draw("b.png","png","dot")
-					# g.draw("a.png","png","dot")
-					print fetchInstruction(gadget)
-					print ""
-					print pat
-					print "Press enter to load next gadget"
-					raw_input()
-			else:
-				print "known"
+	try:
+		numGadget,gadgets = displayGadget(flowfile,"b.png",min_instr=0,min_mem_in = 0, count = False,inspect=False)
+		if inspect and numGadget:
+			if any((GadgetType.Unknown,GadgetClass.Unknown)== gclass for _,gclass,_,_ in gadgets):
+				g.draw("a.png","png","dot")
+			for gadget,gclass,rootinsn,leafinsn in gadgets:
+				print rootinsn,leafinsn
+				if gclass == (GadgetType.Unknown,GadgetClass.Unknown):
+				# if 1==1:
+					
+					
+					pat = getPattern(gadget)
+					seenPattern[pat] = seenPattern.get(pat,0)+1
+					if seenPattern.get(pat,0) == 1:
+						gadget.draw("b.png","png","dot")
+						# g.draw("a.png","png","dot")
+						print fetchInstruction(gadget)
+						print ""
+						print pat
+						print "Press enter to load next gadget"
+						raw_input()
+				else:
+					print "known"
 
-	if not numGadget:
-		if moveNChosen:
-			dstFolder = os.path.join(dfolder,moveFolder)
+		if not numGadget:
+			if moveNChosen:
+				dstFolder = os.path.join(dfolder,"fail")
+				if not os.path.exists(dstFolder):
+					os.makedirs(dstFolder)
+				shutil.move(flowfile,dstFolder)
+			return 0
+		else:
+			if moveNChosen:
+				dstFolder = os.path.join(dfolder,"pass")
 			if not os.path.exists(dstFolder):
 				os.makedirs(dstFolder)
 			shutil.move(flowfile,dstFolder)
-		return 0
 
-	
-	
-	print flowfile.rsplit("/")[-1].rsplit("\\")[-1],numGadget, len([ga for ga in g.nodes() if ga.attr["shape"]=="box"]), [len([ga for gb in ga[0].nodes() if gb.attr["shape"]=="box"]) for ga in gadgets]
-	
-	if inspect:
-		print "Done processing flow <Enter> for next flow"
-		#raw_input()
-	return numGadget
-	
+		
+		if inspect:
+			print flowfile.rsplit("/")[-1].rsplit("\\")[-1],numGadget, len([ga for ga in g.nodes() if ga.attr["shape"]=="box"]), [len([ga for gb in ga[0].nodes() if gb.attr["shape"]=="box"]) for ga in gadgets]
+		
+		if inspect:
+			print "Done processing flow <Enter> for next flow"
+			#raw_input()
+		return numGadget
+	except Exception, e:
+		print "errerr",flowfile
+		print e
+		import traceback
+		traceback.print_exc()
+		#raise Exception()
+		errFolder = os.path.join(dfolder,"error")
+		if not os.path.exists(errFolder):
+			os.makedirs(errFolder)
+		shutil.move(flowfile,errFolder)
+		print "file moved to", errFolder
 
 
-# def process_star(inpt):
-# 	#print 
-# 	inpt = (inpt[0],) + inpt[1]
-# 	return process(*inpt)
+def process_star(inpt):
+	#print 
+	inpt = (inpt[0],) + inpt[1]
+	return process(*inpt)
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
@@ -127,19 +152,16 @@ if __name__ == '__main__':
 				
 				
 				
-	# 		q.append(flowfile)
+			# q.append(flowfile)
 
-	# pool = multiprocessing.Pool(200)
+	# pool = multiprocessing.Pool(1)
 	# #http://stackoverflow.com/a/28463266
 
-	# #for k in itertools.izip(q, itertools.repeat((str(moveNChosen),dfolder,moveFolder))):
+	#for k in itertools.izip(q, itertools.repeat((str(moveNChosen),dfolder,moveFolder))):
 	# #	print k
-	# try:
-	# 	result = pool.map(process_star,itertools.izip(q, itertools.repeat((moveNChosen,dfolder,moveFolder))))
-	# 	pool.close()
-	# 	pool.join()
-	# except KeyboardInterrupt:
-	# 	pool.terminate()
+	# result = pool.map(process_star,itertools.izip(q, itertools.repeat((moveNChosen,dfolder,moveFolder,inspect))))
+	# pool.close()
+	# pool.join()
 
 	# totalNumGadgets = sum(result)
 	print "totalNumGadgets",totalNumGadgets
